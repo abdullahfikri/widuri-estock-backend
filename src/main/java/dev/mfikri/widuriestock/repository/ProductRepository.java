33433dd59ed913dbfcbb00aef3baec7b9a702c90
muhammad fikri <mfikri.dev@gmail.com>
2025-09-15 08:10:23 +0700

    perf(product-service): Tuning SQL query to improve the request time of getting list of product.
    - Use deferred joins to improve retrieval of product data list in deep paggination.
    - Simplify the ProductSummary interface.
    - Refactor the getList method since the Interface is changing.

--- src/main/java/dev/mfikri/widuriestock/repository/ProductRepository.java
+++ src/main/java/dev/mfikri/widuriestock/repository/ProductRepository.java
@@ -1,20 +1,33 @@
 package dev.mfikri.widuriestock.repository;
 
-import dev.mfikri.widuriestock.entity.product.Category;
 import dev.mfikri.widuriestock.entity.product.Product;
-import dev.mfikri.widuriestock.entity.product.ProductPhoto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     boolean existsByName(String name);
 
-    Page<ProductSummary> findBy(Pageable pageable);
+    @Query(
+            value = "SELECT " +
+                    "    p.id as id, " +
+                    "    p.name as name, " +
+                    "    p.description as description, " +
+                    "    c.id as categoryId, " +
+                    "    c.name as categoryName, " +
+                    "    (SELECT pp.image_location FROM product_photos pp WHERE pp.product_id = p.id ORDER BY pp.id ASC LIMIT 1) as imageLocation " +
+                    "FROM products p " +
+                    "JOIN (SELECT id FROM products ORDER BY name LIMIT :#{#pageable.offset}, :#{#pageable.pageSize}) AS p_page ON p.id = p_page.id " +
+                    "JOIN categories c ON p.category_id = c.id " +
+                    "ORDER BY p.name ASC",
+            countQuery = "SELECT count(*) FROM products p",
+            nativeQuery = true
+
+    )
+    Page<ProductSummary> findProductListView(Pageable pageable);
 }
 
--- src/main/java/dev/mfikri/widuriestock/repository/ProductSummary.java
+++ src/main/java/dev/mfikri/widuriestock/repository/ProductSummary.java
@@ -1,13 +1,10 @@
 package dev.mfikri.widuriestock.repository;
 
-
-import dev.mfikri.widuriestock.entity.product.Category;
-import dev.mfikri.widuriestock.entity.product.ProductPhoto;
-
 public interface ProductSummary {
     Integer getId();
     String getName();
     String getDescription();
-    Category getCategory();
-    ProductPhoto getProductPhotos();
+    Integer getCategoryId();
+    String getCategoryName();
+    String getImageLocation();
 }
